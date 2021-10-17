ac34fce95e37fd68db23541fb5e2b75313ae4e47
andrecoco <andrebermudesviana@gmail.com>
2021-10-17 11:24:16 -0300

    fix small bugs

--- db.py
+++ db.py
@@ -128,9 +128,7 @@ def insere_img_transparente(update):
     name = update.message.text
     name = name.replace('/insert_ti', '').strip()
     fileid = update.message.reply_to_message.document.file_id
-    if(fileid is not None and len(fileid) > 0):
-        fileid = fileid[0].file_id
-    else:
+    if(fileid is None or len(fileid) > 0):
         print("Sem imagem para inserir...")
         return
     
