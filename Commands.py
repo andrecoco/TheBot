6d47acacccb7a7adbe4a5efa31e8ed701ba9bafb
andrecoco <andrebermudesviana@gmail.com>
2021-10-27 15:56:49 -0300

    testing logs

--- Commands.py
+++ Commands.py
@@ -74,8 +74,6 @@ def merge_transparent_image(update, context):
             return
         florto.paste_image(media, context)
         image = open('./res/new.png', 'rb')
-        print(os.system('file ./res/front'))
-        print(os.system('file ./res/back'))
         update.message.reply_photo(photo=image)
         florto.clear()
     else:
